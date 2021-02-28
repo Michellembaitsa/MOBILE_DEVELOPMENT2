d668f20235908bfa6ed9ead71af11794e3fac16e
Michellembaitsa <michellembaitsaa@gmail.com>
2021-02-28 13:20:24 +0300

    Assignment2

---
+++ src/Assignment2.kt
@@ -0,0 +1,29 @@
+fun main() {
+    name()
+    modulus()
+    sum()
+    facts()
+
+}
+fun name(){
+    var name="Hello Michelle"
+    print(name)
+}
+fun modulus(){
+    var num1=19
+    var num2=4
+    var modulus=num1%num2
+    print(modulus)
+}
+fun sum(){
+    var num1=19
+    var num2=4
+    var num3=30
+    var num4=89
+    var sum=num1+num2+num3+num4
+    print(sum)
+}
+fun facts(){
+    var facts="I like cats"
+    print(facts)
+}
